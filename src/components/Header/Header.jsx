1a156d70b284af2401d1945d28e8e4a44a418cc4
IgorBezusenko <bezusenko_98@mail.ru>
2021-06-07 09:45:04 +0300

    Added any style to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
+import css from "./Header.module.css"
+import {Grid, Home, LogIn, ShoppingCart} from "react-feather";
+
 export const Header: React.FC = () => {
-    return (<>
-        <h1>BIS!</h1>
-        <ul>
-            <li><Link to={"/"}>Main</Link></li>
-            <li><Link to={"/items"}>Items</Link></li>
-            <li><Link to={"/cart"}>Cart</Link></li>
-            <li><Link to={"/login"}>Login</Link></li>
+    return (<div className={css.header + " " + css.row}>
+        <h2>BIS!</h2>
+        <ul className={css.list + " " + css.row}>
+            <li><Home/><Link to={"/"}>Main</Link></li>
+            <li><Grid/><Link to={"/items"}>Items</Link></li>
+            <li><ShoppingCart/><Link to={"/cart"}>Cart</Link></li>
+            <li><LogIn/><Link to={"/login"}>Login</Link></li>
         </ul>
-    </>)
+    </div>)
 }
