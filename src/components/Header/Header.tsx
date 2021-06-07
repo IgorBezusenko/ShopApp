e8ac03764010178c61fe2b94211e3ea721cbd0a7
IgorBezusenko <bezusenko_98@mail.ru>
2021-06-07 15:28:59 +0300

    updated component Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import {Link} from "react-router-dom";
-
-import css from "./Header.module.css"
 import {Grid, Home, LogIn, ShoppingCart} from "react-feather";
 
+import css from "./Header.module.css"
+
 export const Header: React.FC = () => {
-    return (<div className={css.header + " " + css.row}>
+    return (<div className={css.header}>
         <h2>BIS!</h2>
         <ul className={css.list + " " + css.row}>
-            <li><Home/><Link to={"/"}>Main</Link></li>
-            <li><Grid/><Link to={"/products"}>Products</Link></li>
-            <li><ShoppingCart/><Link to={"/cart"}>Cart</Link></li>
-            <li><LogIn/><Link to={"/login"}>Login</Link></li>
+            <li><Link className={css.row} to={"/"}><Home className={css.icon}/><p>Main</p></Link></li>
+            <li><Link className={css.row} to={"/products"}><Grid className={css.icon}/><p>Products</p></Link></li>
+            <li><Link className={css.row} to={"/cart"}><ShoppingCart className={css.icon}/><p>Cart</p></Link></li>
+            <li><Link className={css.row} to={"/login"}><LogIn className={css.icon}/><p>Login</p></Link></li>
         </ul>
     </div>)
 }
